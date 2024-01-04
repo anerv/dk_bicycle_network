18200e2d6d721b587334f7f265061ee9b2d84f11
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-01-04 16:42:11 +0100

    first clean up

---
+++ scripts/sql/xx_clean_up.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS osm_roads;
+
+DROP TABLE IF EXISTS planet_osm_line;
+
+DROP TABLE IF EXISTS planet_osm_point;
+
+DROP TABLE IF EXISTS planet_osm_polygons;
+
+DROP TABLE IF EXISTS planet_osm_rels;
+
+DROP TABLE IF EXISTS planet_osm_ways;
+
+DROP SCHEMA IF EXISTS matching_geodk_osm CASCADE;
+
+DROP SCHEMA IF EXISTS matching_geodk_osm_no_bike CASCADE;
