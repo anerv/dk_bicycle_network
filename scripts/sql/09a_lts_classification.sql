7f76672a4fda9d41e562bdad87aea332aa02c517
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-01-09 18:15:56 +0100

    added todos

--- scripts/sql/09a_lts_classification.sql
+++ scripts/sql/09a_lts_classification.sql
@@ -3,14 +3,26 @@ ALTER TABLE
 ADD
     COLUMN lts INTEGER DEFAULT NULL;
 
-UPDATE
-    osm_road_edges
-SET
-    lts = 999
-WHERE
-    car_traffic IS FALSE
-    AND cycling_allowed IS FALSE;
-
+-- FIX: highway=cycleway and cycleway(both,left,right) = lane --> categorized as unprotected lane (bicycle category issue)
+-- FIX: paths with cycling allowed but no cycling class - should they also be 1? (bicycle category/bicycle infra issue)
+-- FIX: highway = footway, bicycle = yes, cycling_allowed = yes/true, but bicycle_infra = false? and no category (bicycle category/bicycle infra issue - change throughout!!)
+-- Fix: same, but classified as lanes because of geodk (order? should be overwritten) (bicycle category issue)
+-- FIX highway = pedestrian and bicycle road = yes (bicycle category issue)
+-- FIX: highway = cycleway but also cyclestreet - which category? Which LTS? (bicycle cat issue)
+--
+-- RERUN AND FIX ALL BICYCLE CLASSIFICATION (BUT KEEP MATCHED!)
+-- BEFORE RERUNNING LTS: MAKE SURE THAT BICYCLE CLASSES MAKE SENSE
+--
+-- TODO: deal with highway=footway bicycle crossings (LTS issue)
+-- TODO: deal with highway = 'cycleway' and bicycle class is 2 or 3 (e.g. because of crossing) (LTS issue)
+-- FIX highway=footway and category = crossing (LTS issue)
+-- FIX highway=pedestrian and category = crossing (LTS issue)
+-- FIX highway = pedestrian and cycleway = lane (LTS issue)
+-- fix highway = track and category = lane (because of matching) (LTS issue) -- lts mixed traffic
+-- residential, 5 lanes, bicycle lanes (LTS issue)
+-- FIX: why no lts 2 lol?
+-- FIX: too many in lts 4??
+--
 --Assign bicycle class
 UPDATE
     osm_road_edges
@@ -26,7 +38,9 @@ SET
         WHEN bicycle_category = 'shared_lane' THEN 3
         WHEN bicycle_category IS NULL
         AND cycling_allowed IS TRUE THEN 3
-    END;
+    END
+WHERE
+    bicycle_allowed IS TRUE;
 
 DO $$
 DECLARE
@@ -62,17 +76,17 @@ SET
 WHERE
     (
         bicycle_class = 2
-        AND maxspeed_assumed = < 40
+        AND maxspeed_assumed <= 40
     )
     OR (
         bicycle_class = 2
         AND (
             centerline_assumed IS TRUE
             AND (
-                lanes_assumed = < 4
+                lanes_assumed <= 4
                 AND oneway NOT IN ('yes', 'True', 'true', '1')
                 OR (
-                    lanes_assumed = < 2
+                    lanes_assumed <= 2
                     AND oneway IN ('yes', 'True', 'true', '1')
                 )
             )
@@ -87,13 +101,13 @@ WHERE
     bicycle_class = 3
     AND (
         centerline_assumed IS TRUE
-        AND maxspeed_assumed = < 50
-        AND lanes_assumed = < 3
+        AND maxspeed_assumed <= 50
+        AND lanes_assumed <= 3
     )
     OR (
         centerline_assumed IS FALSE
-        AND maxspeed_assumed = < 40
-        AND lanes_assumed = < 2
+        AND maxspeed_assumed <= 40
+        AND lanes_assumed <= 2
     );
 
 -- *** LTS 3 ***
@@ -128,9 +142,9 @@ WHERE
         OR (
             centerline_assumed IS FALSE
             AND (
-                lanes_assumed = < 4
+                lanes_assumed <= 4
                 OR (
-                    lanes_assumed = < 2
+                    lanes_assumed <= 2
                     AND oneway IN ('yes', 'True', 'true', '1')
                 )
             )
@@ -146,15 +160,15 @@ WHERE
     AND (
         centerline_assumed IS FALSE
         AND (
-            assumed_lanes = < 3
-            OR maxspeed_assumed = < 50
+            lanes_assumed <= 3
+            OR maxspeed_assumed <= 50
         )
     )
     OR (
         centerline_assumed IS TRUE
         AND (
-            assumed_lanes = < 4
-            OR maxspeed_assumed = < 70
+            lanes_assumed <= 4
+            OR maxspeed_assumed <= 70
         )
     );
 
@@ -198,33 +212,7 @@ SET
     lts = 1
 WHERE
     bicycle_class = 1
-    AND bicycle_infrastructure_final = TRUE -- AND bicycle_surface_assumed IN (
-    --     'asphalt',
-    --     'bricks',
-    --     'chipseal',
-    --     'cobblestone',
-    --     'compacted',
-    --     'concrete',
-    --     'concrete:lanes',
-    --     'concrete:plates',
-    --     'fine_gravel',
-    --     'grass_paver',
-    --     'metal',
-    --     'metal_grid',
-    --     'paved',
-    --     'paving_stones',
-    --     'pebblestone',
-    --     'plastic',
-    --     'sett',
-    --     'sp',
-    --     'steel',
-    --     'stone',
-    --     'tartan',
-    --     'tree',
-    --     'unpaved',
-    --     'wood'
-    -- )
-;
+    AND bicycle_infrastructure_final = TRUE;
 
 -- *** LTS '5' WHERE cycling is not allowed but car traffic is **
 UPDATE
@@ -246,11 +234,77 @@ BEGIN
         osm_road_edges
     WHERE
         lts IS NULL
-        AND car_traffic IS TRUE
-        OR (
-            cycling_allowed IS TRUE
-            AND surface_assumed NOT IN ('gravel', 'grass')
+        AND (
+            car_traffic IS TRUE
+            OR bicycle_category IS NOT NULL
         );
 
-ASSERT bike_class_null = 0,
+ASSERT lts_null = 0,
 'Edges missing LTS value';
+
+END $$;
+
+DO $$
+DECLARE
+    lts_error INT;
+
+BEGIN
+    SELECT
+        COUNT(*) INTO lts_error
+    FROM
+        osm_road_edges
+    WHERE
+        lts IS NOT NULL
+        AND (
+            car_traffic IS FALSE
+            AND bicycle_category IS NULL
+        );
+
+ASSERT lts_error = 0,
+'Edges with surplus LTS value';
+
+END $$;
+
+UPDATE
+    osm_road_edges
+SET
+    lts = 999
+WHERE
+    car_traffic IS FALSE
+    AND cycling_allowed IS FALSE;
+
+--make sure paths and tracks with bad surface or bicycle no are not included
+UPDATE
+    osm_road_edges
+SET
+    lts = 999
+WHERE
+    bicycle_infrastructure_final IS FALSE
+    AND highway IN ('path', 'track');
+
+--bicycle_surface_assumed IN (
+--     'asphalt',
+--     'bricks',
+--     'chipseal',
+--     'cobblestone',
+--     'compacted',
+--     'concrete',
+--     'concrete:lanes',
+--     'concrete:plates',
+--     'fine_gravel',
+--     'grass_paver',
+--     'metal',
+--     'metal_grid',
+--     'paved',
+--     'paving_stones',
+--     'pebblestone',
+--     'plastic',
+--     'sett',
+--     'sp',
+--     'steel',
+--     'stone',
+--     'tartan',
+--     'tree',
+--     'unpaved',
+--     'wood'
+-- )
