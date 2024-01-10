65463acaeb8a9e04a1941e20944ec6ce54ccdc47
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-01-10 15:09:40 +0100

    working on export

--- scripts/sql/10b_export.sql
+++ scripts/sql/10b_export.sql
@@ -1,31 +1,45 @@
--- update km?
--- update kmh?
--- update cost?
--- update reverse cost?
--- columns to include
--- routing cols above?
--- x1, etc
--- bicycle infrastructure
--- bicycle infra final
--- geodk surface
--- geodk category
--- lts
--- surface assumed
--- lit
--- maxspeed
--- surface
--- cycleway:surface
--- maxspeed assumed
--- lanes assumed
--- centerline assumed
--- highway
-keep pgrouting,
-cycling,
-surface,
-speed,
-matching,
-assumed_tags,
-lts -
-OR remove matching
-OR org tags ?
-)
+-- TODO: update km
+-- TODO: update cost (based on km and kmh)
+-- TODO: update reverse cost
+-- looking for edges that have been split!
+ALTER TABLE
+    osm_road_edges
+ADD
+    COLUMN LENGTH NUMERICAL DEFAULT NULL;
+
+UPDATE
+    osm_road_edges
+SET
+    LENGTH = ST_length(geometry);
+
+SELECT
+    id,
+    osmid,
+    x1,
+    y1,
+    x1,
+    x2,
+    source,
+    target,
+    cost,
+    reverse_cost,
+    highway,
+    bicycle_infrastructure AS bicycle_infrastructure_osm,
+    bicycle_infrastructure_final,
+    matched,
+    geodk_category,
+    bicycle_category,
+    bicycle_class,
+    along_street,
+    cycleway_shared,
+    cycling_allowed,
+    car_traffic,
+    lts,
+    lit,
+    maxspeed_assumed,
+    lanes_assumed,
+    bicycle_surface_assumed,
+    centerline_assumed,
+    geometry
+FROM
+    osm_road_edges;
