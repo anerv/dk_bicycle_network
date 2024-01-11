c5ff64785736dfdc8ed98e64687009615e22e909
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-01-11 18:22:47 +0100

    added nodes

--- scripts/sql/10b_export.sql
+++ scripts/sql/10b_export.sql
@@ -1,63 +1,63 @@
--- TODO: update km
--- TODO: update cost (based on km and kmh)
--- TODO: update reverse cost
--- looking for edges that have been split!
-ALTER TABLE
+-- UPDATE km and cost to match default cycling speed of 15 km/h
+UPDATE
     osm_road_edges
-ADD
-    COLUMN edge_length NUMERICAL DEFAULT NULL;
+SET
+    km = ST_length(geometry),
+    kmh = 15;
 
 UPDATE
     osm_road_edges
 SET
-    edge_length = ST_length(geometry);
+    cost = km * kmh,
+    reverse_cost = km * kmh;
 
-UPDATE
-    osm_road_edges
-SET
-    km = edge_length
-WHERE
-    km <> edge_length;
+CREATE VIEW osm_edges_export AS (
+    SELECT
+        id,
+        osmid,
+        x1,
+        y1,
+        x1,
+        x2,
+        source,
+        target,
+        km,
+        kmh,
+        cost,
+        reverse_cost,
+        highway,
+        NAME,
+        bicycle_infrastructure AS bicycle_infrastructure_osm,
+        bicycle_infrastructure_final,
+        matched,
+        geodk_category,
+        bicycle_category,
+        bicycle_class,
+        along_street,
+        cycleway_shared,
+        cycling_allowed,
+        car_traffic,
+        lit,
+        maxspeed_assumed,
+        lanes_assumed,
+        bicycle_surface_assumed,
+        centerline_assumed,
+        urban,
+        lts,
+        geometry
+    FROM
+        osm_road_edges
+);
 
--- TODO: look at kmh - should match maxspeed assumed? use maxspeed assumed instead?
--- UPDATE
---     osm_road_edges
--- SET
---     cost = km * kmh;
--- UPDATE
---     osm_road_edges
--- SET
---     cost_reverse = ? ? ?;
-SELECT
-    id,
-    osmid,
-    x1,
-    y1,
-    x1,
-    x2,
-    source,
-    target,
-    cost,
-    reverse_cost,
-    highway,
-    NAME,
-    bicycle_infrastructure AS bicycle_infrastructure_osm,
-    bicycle_infrastructure_final,
-    matched,
-    geodk_category,
-    bicycle_category,
-    bicycle_class,
-    along_street,
-    cycleway_shared,
-    cycling_allowed,
-    car_traffic,
-    lts,
-    lit,
-    maxspeed_assumed,
-    lanes_assumed,
-    bicycle_surface_assumed,
-    centerline_assumed,
-    urban,
-    geometry
-FROM
-    osm_road_edges;
+CREATE VIEW osm_nodes_export AS (
+    SELECT
+        id,
+        osm_id,
+        intersection_type,
+        highway,
+        crossing,
+        node_degree,
+        geometry
+    FROM
+        nodes
+);
