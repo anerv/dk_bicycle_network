9c4e0be2fa9ffb96fd514f94eeca233bbb672f32
Ane Rahbek Vierø <vieroe@hotmail.com>
2024-01-16 16:15:28 +0100

    first v

--- scripts/10_clean_up_export.py
+++ scripts/10_clean_up_export.py
@@ -36,3 +36,53 @@ connection.close()
 print("Clean up done!")
 
 # %%
+connection = dbf.connect_pg(db_name, db_user, db_password, db_port, db_host=db_host)
+
+dbf.run_query_pg(
+    "sql/10b_export.sql",
+    connection,
+    success="Query successful!",
+    fail="Query failed!",
+    commit=True,
+    close=False,
+)
+
+connection.close()
+
+print("Export views ready!")
+
+#%%
+connection = dbf.connect_pg(db_name, db_user, db_password, db_port, db_host=db_host)
+
+q = "SELECT * FROM osm_edges_export;"
+
+edges = dbf.run_query_pg(
+    q,
+    connection,
+    success="Query successful!",
+    fail="Query failed!",
+    commit=True,
+    close=False,
+)
+
+edges.to_parquet("../data/processed/osm_road_edges.parquet")
+
+del edges
+
+q = "SELECT * FROM osm_nodes_export;"
+
+nodes = dbf.run_query_pg(
+    q,
+    connection,
+    success="Query successful!",
+    fail="Query failed!",
+    commit=True,
+    close=False,
+)
+
+nodes.to_parquet("../data/processed/osm_road_nodes.parquet")
+
+connection.close()
+
+print("Tables exported!")
+#%%
